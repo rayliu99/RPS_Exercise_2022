445ad3712cc590cd282966e1cb13da7a4fca2412
Shurui Liu <sl1760@georgetown.edu>
2022-01-29 16:28:45 -0500

    Asked for user input

--- game.py
+++ game.py
@@ -5,3 +5,25 @@
 # todo: write some Python code here to satisfy the exercise objectives
 # ... https://github.com/prof-rossetti/intro-to-python/blob/main/exercises/rock-paper-scissors/README.md
 #
+
+
+
+
+# Ask for user input
+u = input("Please choose one of: 'rock', 'paper', 'scissors': ")
+
+print("USER CHOSE:", u)
+
+
+ # validations
+
+
+ # computer choice
+
+
+ # determine the winner
+
+
+ # final results 
+
+
